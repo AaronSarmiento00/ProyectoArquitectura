72409f71795fdee02a7bb9eb85859e0a14fd49c1
sergioh15 <92654987+sergioh15@users.noreply.github.com>
2023-09-20 07:53:24 -0500

    Delete demoArquitectura - copia directory
    cambio

--- demoArquitectura - copia/src/main/java/com/example/demoarquitectura/repository/crud/ShoppingCartItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demoarquitectura.repository.crud;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShoppingCartItemRepository extends CrudRepository<ShoppingCartItem, Long> {
-}
