f72689048dc20a94c7d55175e2bd9f372b1f4c74
sergioh15 <92654987+sergioh15@users.noreply.github.com>
2023-09-20 08:02:25 -0500

    Carrito compras
    de prueba

---
+++ demoArquitectura - copia - Carrito/src/main/java/com/example/demoarquitectura/repository/crud/ShoppingCartItemRepository.java
@@ -0,0 +1,6 @@
+package com.example.demoarquitectura.repository.crud;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ShoppingCartItemRepository extends CrudRepository<ShoppingCartItem, Long> {
+}
